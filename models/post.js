74f58b3e33544a0033a6b2c02f3dbf04b855426f
chrynx <ralphmadriaga@gmail.com>
2017-09-12 09:21:33 +0100

    Pushing to heroku

--- models/post.js
+++ models/post.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
   text: { type: String, required: true },
-  rating: Number,
+  rating: String,
   user: String
 });
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,25 @@
 console.log('Hello world');
+const comment = require('../models/post');
+const $ratingColor = $('.rating');
+const rating = [
+  'ridiculous',
+  'bad',
+  'meh',
+  'decent',
+  'spectacular'
+];
+if(comment.rating === rating[0]){
+  $ratingColor.css('background-color: red');
+}
+if(comment.rating === rating[1]){
+  $ratingColor.toggleClass('bad');
+}
+if(comment.rating === rating[2]){
+  $ratingColor.toggleClass('meh');
+}
+if(comment.rating === rating[3]){
+  $ratingColor.toggleClass('decent');
+}
+if(comment.rating === rating[4]){
+  $ratingColor.toggleClass('spectacular');
+}
